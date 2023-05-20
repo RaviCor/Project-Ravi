4f771269819d5bed43602b1b1938dfede1a49e53
Joaquin Cordisco <joaquincordisco@gmail.com>
2023-05-19 22:33:59 -0300

    First Commit

---
+++ landing_page/script.js
@@ -0,0 +1,6 @@
+document.getElementById("contactForm").addEventListener("submit", function(event) {
+    event.preventDefault();
+    // Aquí puedes agregar la lógica para el envío del formulario de contacto
+    // Por ejemplo, puedes usar JavaScript para validar los datos ingresados y enviarlos a través de una solicitud al servidor
+  });
+  
