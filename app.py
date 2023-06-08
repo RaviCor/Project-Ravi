c51cf72869339e2ad7ad7d22a2adfb4e0a7f4d58
Joaquin Cordisco <joaquincordisco@gmail.com>
2023-06-07 23:39:08 -0300

    blueprint auth funtional

--- app.py
+++ app.py
@@ -1,33 +1,48 @@
 from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
-from config import DevConfig
+from config import DevConfig, ProdConfig
+from flask_login import LoginManager
 
 db = SQLAlchemy()
+login_manager = LoginManager()
 
-def create_app():
+def create_app(enviroment):
     app = Flask(__name__)
-    app.config.from_object(DevConfig)
+    
+    if "DevConfig" in enviroment:
+        app.config.from_object(DevConfig)
+    elif "ProdConfig" in enviroment:
+        app.config.from_object(ProdConfig)
+    
     db.init_app(app)
+    login_manager.init_app(app)
 
-    from models import user
+    from models.user import User
 
     with app.app_context():
         inspector = db.inspect(db.engine)
         existing_tables = inspector.get_table_names()
         check_tables = all(table in existing_tables for table in db.metadata.tables.keys())
-        print(check_tables)
         if not check_tables:
-            print("Creando tablas")
+            print("Creando tablas para la base de datos.")
             db.create_all()
 
     @app.route('/')
     def index():
         return render_template("index.html")
 
+    with app.app_context():
+        from auth.auth import auth as auth_blueprint
+        app.register_blueprint(auth_blueprint, url_prefix='/auth')
+
+    @login_manager.user_loader
+    def load_user(user_id):
+        return User.get(user_id)
+    
+    print(app.blueprints)
+
     return app
 
-
 if __name__ == "__main__":
-    app = create_app()
+    app = create_app("DevConfig")  # Pasa la configuración como argumento
     app.run()
-
--- models/user.py
+++ models/user.py
@@ -1,9 +1,20 @@
 from app import db
+from flask_login import UserMixin
+from werkzeug.security import generate_password_hash, check_password_hash
 
-class User(db.Model):
+class User(db.Model, UserMixin):
     __tablename__ = 'user'
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
 
     def __repr__(self):
         return f"<User {self.username}>"
+    def set_password(self, password):
+        self.password = generate_password_hash(password)
+    def check_password(self, password):
+        return check_password_hash(self.password, password)
+    def __repr__(self):
+        return '<User {}>'.format(self.email)
+    @classmethod
+    def get(cls, user_id):
+        return cls.query.get(user_id)
